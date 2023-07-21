b6c0b408ad735dbb1ecd87c297df06c72c2d0f2e
mannyk <man962@yahoo.com>
2023-07-21 11:01:18 -0700

    move Robustness to app.py.
    1. test logs/rot10E-0721 and logs/xlat10E-0721
    a) both pass Generalization (orbit_loss) test at 100%.
    b) rot10E pass the adversarial robustness test, xlat10E is close but
    the final distance = 0.109. Just over margin=.1

--- bispectral_networks/experiments/translation_experiment.py
+++ bispectral_networks/experiments/translation_experiment.py
@@ -34,7 +34,7 @@ from mk_mlutils.utils import torchutils
 from bispectral_networks import projconfig
 from bispectral_networks.MNIST_inv_eq import MNISTExemplars, MNIST_train_inv_eq_set
 import bispectral_networks.MNIST_inv_eq as MNIST_inv_eq
-from bispectral_networks.experiments.rotation_experiment import Weights
+from bispectral_networks.experiments.rotation_experiment import Weights, Robustness
 #from rotation_experiment_analysis import Weights, Equivariance, Invariance, Generalization, Robustness
 
 kMNIST_path = None	#"datasets/mnist/mnist_train.csv"
@@ -161,69 +161,6 @@ def Generalization(
 	time_spent(start, f"Generalization: ", count=1)
 
 
-def Robustness(
-	checkpoint:torch.nn.Module,
-	patch_size:int,
-	save_dir:str,
-	device="cpu"
-):
-	# ## Robustness Analysis
-
-	# Now, we analyze the robustness of the model through a simple adversarial example experiment. Here, we start with noise as the input, and optimize this input to yield a network output that is as close as possible to the network output for a target image. For this analysis, we select a single exemplar from the MNIST dataset as the target, and run the optimization starting from 100 different noise images to examine the range of results. Note that the BasicGradientDescent method often gets stuck in local minima that are farther from the target embedding than desired to be considered a meaningful "adversarial example." While such runs are interesting regardless, the approach should be rerun until the target reaches the margin. Fancier optimization methods could be used to escape these local minima, but here we use a vanilla model.
-	# 
-	# We find that, for this model, points that are close in embedding space will be (close to) equivalent up to the group action that the model has learned to be invariant to. In this notebook we use a margin of 0.1, which is larger than that used in the paper (since it tends to be difficult to drive the model lower). This means that the examples are more likely to look different from the target. Despite this, we find the same effect, which further demonstrates the robustness of this model.
-	print("Adversarial Robustness")
-	start = time.time()
-
-	from bispectral_networks.analysis.adversary import BasicGradientDescent
-	from bispectral_networks.analysis.plotting import animated_video
-
-	#torchutils.initSeeds(1)		#torch.random.seed()
-
-	# 1 exemplar per digit is randomly selected
-	robustness_dataset = MNIST_train_inv_eq_set(
-		path=kMNIST_path,
-		n_exemplars=1,	# 1 exemplar per digit is randomly selected
-		patch_size=16,
-		fraction_transforms=1.0,
-		xformkind="center",
-		seed=1
-	)
-	print(f" {robustness_dataset.data.shape=}")
-
-	cpudevice = "cpu"
-	target_idx = np.random.randint(len(robustness_dataset.data))
-	target_image = robustness_dataset.data[target_idx].to(cpudevice)
-	target_image_tiled = torch.tensor(np.tile(target_image, (100, 1)))
-
-	plt.imshow(target_image.reshape(patch_size, patch_size), cmap="Greys_r")
-
-	adversary = BasicGradientDescent(model=checkpoint.model, 
-									 target_image=target_image_tiled,
-									 margin=.1,
-									 lr=0.1,
-									 save_interval=10,
-									 print_interval=100,
-									 optimizer=torch.optim.Adam,
-									 device=device)
-
-	x_, target_embedding, embedding = adversary.train(max_iter=1000)
-
-	history = np.array([x.reshape(-1, patch_size, patch_size) for x in adversary.history])
-	history.shape
-
-	image_grid(history[-1], shape=(10, 10), cmap="Greys_r", figsize=(20, 20))
-	plt.savefig(os.path.join(save_dir, "adversary.pdf"))
-
-	time_spent(start, f"Adversial Robustness: ", count=1)
-
-	# **Visualizing the Optimization Process**
-	# 
-	# The below video shows the optimization of a single example image through gradient steps.
-
-	animated_video(history[:, 0], interval=100, cmap="Greys_r")
-
-
 def main(
 	save_dir=kSave_dir,
 	log_path=kLog_path,
@@ -312,8 +249,7 @@ def main(
 		Generalization(checkpoint, patch_size=16, k=25, save_dir=save_dir)
 
 	if kRobustness:
-		Robustness(checkpoint, patch_size, save_dir, device)
-
+		Robustness(checkpoint, patch_size, save_dir, device=device)
 
 
 if __name__ == '__main__':
@@ -323,7 +259,7 @@ if __name__ == '__main__':
 	# - Invariance and Equivariance
 	# - Generalization
 	# - Robustness
-	rotation_experiment.main(
+	translation_experiment.main(
 		log_path=kLog_path,
 		save_dir=kSave_dir,
 		kWeights=False,
